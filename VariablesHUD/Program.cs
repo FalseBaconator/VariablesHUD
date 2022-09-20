9866d601e4de37f20d922c7ed8816e90d5cfc8b2
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-20 10:03:37 -0300

    Made the base variables

--- VariablesHUD/Program.cs
+++ VariablesHUD/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace VariablesHUD
 {
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            string studio = "Fake Bacon Games";
+            string title = "Not A Real Game";
+            int score = 0;
+            int enemyScore = 10;
+            int scoreMulitplyer = 1;
+            int HP = 100;
+            int HPMax = 100;
+            int enemyDMG = 10;
+            int healthKit = 25;
+            int lives = 3;
+            int loseLife = 1;
+            int oneUp = 1;
+            float shield = 0.0f;
+            float shieldDMGReduce = 2.0f;
+            float shieldFull = 10.0f;
+
+            
+
         }
     }
 }
